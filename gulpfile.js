a552c31a5bb983cbbec222158905fdf26cf3d01c
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-06-16 09:37:40 -0500

    Added favicons. Optimzed images.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+gulp.task('imagemin', function() {
+   gulp.src('./images/*.*')
+      .pipe(imagemin({svgoPlugins: [{removeViewBox: false}]}))
+      .pipe(gulp.dest('./img'));
+});
 
 gulp.task('sass', function() {
    gulp.src(['./styles/**/*.scss', '!./styles/common/fa430/scss/*.scss'])
